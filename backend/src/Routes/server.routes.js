cb4666f0598127075e9fb95519a7dac569e83b00
helloayan14 <ayansharma2006@gmail.com>
2025-04-15 22:46:42 +0530

    added router

--- backend/src/Routes/server.routes.js
+++ backend/src/Routes/server.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { submitaserver,deleteServer,editServer ,getallservers} from "../controllers/server.controller.js";
+import { submitaserver,deleteServer,editServer ,getallservers, getserverbyid} from "../controllers/server.controller.js";
 
 import { verifyjwt } from "../middlewares/auth.middleware.js";
 
@@ -9,5 +9,5 @@ router.route("/submit").post(verifyjwt,submitaserver)
 router.route("/edit/:serverid").put(verifyjwt,editServer)
 router.route("/delete/:serverid").delete(verifyjwt,deleteServer)
 router.route("/allserver").get(getallservers)
-
+router.route("/:serverid").get(getserverbyid)
 export default router;
