38f9c124238caeec1bf1db5341021cb6d60858ff
helloayan14 <ayansharma2006@gmail.com>
2025-04-30 11:58:04 +0530

    changed / route req

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,9 +20,10 @@ app.use(express.json({limit: '50mb'}))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
 app.use(cookieParser())
 
-app.get('/', (res) => {
+app.get('/', (req, res) => {
     res.send('Welcome to the MCP Central API. Use /api/v1/user or /api/v1/server.');
   });
+  
   
 
 import userRoutes from './Routes/user.routes.js'
